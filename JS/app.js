dcc0ef833376d997cc1512a8c42a908256021050
NhanItJAVA1 <nhantnpt2609@gmail.com>
2024-03-25 23:32:18 +0700

    Begin project

---
+++ JS/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.image').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        slidesToScroll:2,
+        arrows:false,
+        Infinity:true,
+        autoplay:true,
+        autoplaySpeed:1000,
+        dot:true,
+    })
+});
